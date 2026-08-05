90327dc9c5ac5676788bf6dda0485c229a2038a4
jv6hgy9sby-ops <jv6hgy9sby@privaterelay.appleid.com>
2026-08-05 20:05:00 +0300

    Publish final mobile hero asset version

--- nastya-hr/assets/theme-init.js
+++ nastya-hr/assets/theme-init.js
@@ -6,7 +6,7 @@
   var lightColor="#FAF7F2";
   var darkColor="#1F2328";
   var saved=null;
-  var assetVersion="20260805-4";
+  var assetVersion="20260805-5";
 
   try{
     saved=localStorage.getItem(storageKey);
