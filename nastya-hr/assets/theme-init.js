5a9a63bee2b94de0ed51717b5ae0a24402b6ea7a
jv6hgy9sby-ops <jv6hgy9sby@privaterelay.appleid.com>
2026-08-05 21:02:41 +0300

    Refresh final logo cache version

--- nastya-hr/assets/theme-init.js
+++ nastya-hr/assets/theme-init.js
@@ -6,7 +6,7 @@
   var lightColor="#FAF7F2";
   var darkColor="#1F2328";
   var saved=null;
-  var assetVersion="20260805-5";
+  var assetVersion="20260805-logo-final";
 
   try{
     saved=localStorage.getItem(storageKey);
